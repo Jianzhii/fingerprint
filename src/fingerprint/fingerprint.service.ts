febbce720b45a688923835142825027696e70c2f
jz <s.jianzhi97@gmail.com>
2022-09-05 17:14:51 +0800

    add function to check if fpdata exist

--- src/fingerprint/fingerprint.service.ts
+++ src/fingerprint/fingerprint.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { rmdir } from 'fs';
+import { WritableStreamDefaultWriter } from 'node:stream/web';
 import { Repository } from 'typeorm';
 import { FpDataEntityModel } from './entities/fpdata.model';
 
@@ -7,7 +9,7 @@ import { FpDataEntityModel } from './entities/fpdata.model';
 export class FingerprintService {
     constructor(
         @InjectRepository(FpDataEntityModel)
-        private fingerPrintRepository: Repository<FpDataEntityModel>,
+        private fpDataRepository: Repository<FpDataEntityModel>,
     ) {}
 
     fpNoJs(request) {
@@ -21,5 +23,77 @@ export class FingerprintService {
         }
 
         return `This action returns all fingerprint`;
+    }
+
+    async checkIfFPExists(
+        id: string,
+        userAgentHttp: string,
+        acceptHttp: string,
+        encodingHttp: string,
+        languageHttp: string,
+        pluginsJsHashed: string,
+        platformJS: string,
+        cookiesJS: string,
+        dntJS: string,
+        timezoneJS: string,
+        resolutionJS: string,
+        localJS: string,
+        sessionJS: string,
+        IEDataJS: string,
+        canvasJsHashed: string,
+        webGLJsHashed: string,
+        fontsFlashHashed: string,
+        resolutionFlash: string,
+        languageFlash: string,
+        platformFlash: string,
+        adBlock: string,
+    ) {
+        const count = await this.fpDataRepository.count({
+            where: {
+                id: id,
+                userAgentHttp: userAgentHttp,
+                acceptHttp: acceptHttp,
+                encodingHttp: encodingHttp,
+                languageHttp: languageHttp,
+                pluginsJsHashed: pluginsJsHashed,
+                platformJS: platformJS,
+                cookiesJS: cookiesJS,
+                dntJS: dntJS,
+                timezoneJS: timezoneJS,
+                resolutionJS: resolutionJS,
+                localJS: localJS,
+                sessionJS: sessionJS,
+                IEDataJS: IEDataJS,
+                canvasJsHashed: canvasJsHashed,
+                webGLJsHashed: webGLJsHashed,
+                fontsFlashHashed: fontsFlashHashed,
+                resolutionFlash: resolutionFlash,
+                languageFlash: languageFlash,
+                platformFlash: platformFlash,
+                adBlock: adBlock,
+            },
+        });
+
+        return count === 1;
+    }
+
+    async checkIfFPWithNoJsExists(
+        id: string,
+        userAgentHttp: string,
+        acceptHttp: string,
+        encodingHttp: string,
+        languageHttp: string,
+    ) {
+        const count = await this.fpDataRepository.count({
+            where: {
+                id: id,
+                userAgentHttp: userAgentHttp,
+                acceptHttp: acceptHttp,
+                encodingHttp: encodingHttp,
+                languageHttp: languageHttp,
+            },
+        });
+
+        return count === 1;
     }
 }
